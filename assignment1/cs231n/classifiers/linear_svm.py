92a74a78ad9161f0c4f7679ac43dcb65cdddb144
UrMBCMRabbont <philipt710mh@gmail.com>
2022-09-25 17:14:09 +0800

    done vectorized loss&gradient function

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -38,7 +38,6 @@ def svm_loss_naive(W, X, y, reg):
                 loss += margin
                 dW[:,y[i]] += -(X[i].T)
                 dW[:,j] += (X[i].T)
-                
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
@@ -68,6 +67,8 @@ Structured SVM loss function, vectorized implementation.
 Inputs and outputs are the same as svm_loss_naive.
 """
 def svm_loss_vectorized(W, X, y, reg):
+    num_train = X.shape[0]
+    num_class = W.shape[1]
     loss = 0.0
     dW = np.zeros(W.shape) # initialize the gradient as zero
 
@@ -99,28 +100,13 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-#     for i in xrange(num_train):
-#         scores = X[i].dot(W)
-#         correct_class_score = scores[y[i]]
-#         for j in xrange(num_classes):
-#             if j == y[i]:
-#                 continue                                 #jump over self class
-#             margin = scores[j] - correct_class_score + 1 # note delta = 1
-#             if margin > 0:
-#                 loss += margin
-#                 dW[:,y[i]] += -(X[i].T)
-#                 dW[:,j] += (X[i].T)
-                
-
-#   # Right now the loss is a sum over all training examples, but we want it
-#   # to be an average instead so we divide by num_train.
-#     loss /= num_train
-#     dW /= num_train
-
-#   # Add regularization to the loss.
-#     loss += reg * np.sum(W * W)  # L2 regularization
-#     dW += reg * 2 * W
-    pass
+    valid_class_count = np.zeros(margin.shape)
+    valid_class_count[margin > 0] = 1
+    valid_class_count[np.arange(valid_class_count.shape[0]), y] -= np.sum(valid_class_count, axis=1)
+    dW = (X.T).dot(valid_class_count)
+    
+    dW /= num_train
+    dW += reg * 2 * W
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
